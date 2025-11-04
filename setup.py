b1ef11566dee53a6c200bdba4c877ea2c1b88ef8
SW33TSTUFF <simaakniyaz@gmail.com>
2025-11-05 03:36:17 +0530

    add requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="1.0.0",
+    author="Simaak",
+    author_email="simaakniyaz@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
