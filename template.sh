356cb54ed5d0b6d64e159cb41d762738dcb0b869
SW33TSTUFF <simaakniyaz@gmail.com>
2025-11-05 03:20:46 +0530

    add folder structure

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating directories
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/___init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch README.md
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Files created succesfully!"
+
+
